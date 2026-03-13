161b7b0e7967bd45433b63a56cb27eefbfbafd29
BerkGozek <berkgzk@bu.edu>
2026-03-13 09:43:49 -0400

    Add 4xx and 5xx error detection

--- analyze_logs.py
+++ analyze_logs.py
@@ -48,10 +48,26 @@ def count_endpoints(logs):
     
     return endpoint_counter
 
+def detect_errors(logs):
+    client_errors = []
+    server_errors = []
+
+    for log in logs:
+        status  = log["status"]
+
+        if 400 <= status < 500:
+            client_errors.append(log)
+        elif 500 <= status <600:
+            server_errors.append(log)
+    
+    return client_errors,server_errors
+
 def main():
     logs = parse_log_file(LOG_FILE)
     status_counts = count_status_codes(logs)
     endpoint_counts = count_endpoints(logs)
+    client_errors, server_errors = detect_errors(logs)
+
 
     _ = subprocess.call('cls' if os.name == 'nt' else 'clear', shell=True)
     
@@ -75,6 +91,30 @@ def main():
 
     for path,count in endpoint_counts.most_common(5):
         print(f"{path}:\t{count} requests")
+
+    print("\n" * 2)
+
+    print("Client errors (4xx)")
+
+    print("=" * 30)
+
+    if client_errors:
+        for error in client_errors:
+            print(f"{error["status"]} {error["method"]} {error["path"]} from {error["ip"]}")
+    else:
+        print("No Client Errors Detected")
+
+    print("\n" * 2)
+
+    print("Server errors (5xx)")
+
+    print("=" * 30)
+
+    if server_errors:
+        for error in server_errors:
+            print(f"{error["status"]} {error["method"]} {error["path"]} from {error["ip"]}")
+    else:
+        print("No Server Errors Detected")
 
 if __name__ == "__main__":
     main()
